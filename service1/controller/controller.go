9a7d9e4ebdeded1108cffb13cd5489b301052ff6
Sumrid Klinkanha <59070174@it.kmitl.ac.th>
2019-06-20 15:10:37 +0700

    update docker-compose.yml

--- service1/controller/controller.go
+++ service1/controller/controller.go
@@ -12,15 +12,16 @@ func Index(c echo.Context) error {
 	return c.HTML(http.StatusOK, "<h1>Hello world form app1</h1>")
 }
 
+// GetApp2 get data from app2
 func GetApp2(c echo.Context) error {
-	res, err := http.Get("http://localhost:3000/")
+	res, err := http.Get("http://localhost:3000")
 	if err != nil {
-		return c.String(500, "Can not get data from app2.")
+		return c.String(500, err.Error())
 	}
 
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return c.String(500, "Can not read data.")
+		return c.String(500, err.Error())
 	}
 
 	// To string
